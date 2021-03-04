d7c875d49f85312aad43273bff77a2c0e91503a4
skordas <skordas@redhat.com>
2021-03-04 15:40:42 -0500

    Node Tuning Operator demo

--- NTO/nto_demo.sh
+++ NTO/nto_demo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Get the file - copy and paste in terminal to get demo file:
+# wget https://raw.githubusercontent.com/skordas/demos/master/NTO/nto_demo.sh
+
+# Run the demo:
+# bash nto_demo.sh
+
 # Colors
 red="\e[31m"
 blue="\e[34m"
@@ -40,11 +46,17 @@ function display_and_run() {
 	read
 }
 
-#test
+function cleaning() {
+	echo -e "         $red#########$blue Cleaning after demo $red#########$reset"
+	oc label pod $pod tuned.openshift.io/elasticsearch-
+	oc project default
+	rm nf-conntrack-max.yaml performance.yaml
+}
+
+# Demo:
 
 clear
 line "Node Tuning Operator"
-clear
 line "Content:"
 indent_line "- Tuned"
 indent_line "- Tuned Node"
@@ -54,7 +66,7 @@ indent_line "- Bonus: NTO as SLO"
 
 clear
 line "TUNED"
-line "Installation"
+line "Installation:"
 indent_line "https://github.com/redhat-performance/tuned"
 line "# RHEL"
 indent_line "$ yum install tuned"
@@ -77,6 +89,47 @@ display_and_run "tuned-adm profile balanced"
 display_and_run "sysctl kernel.sched_autogroup_enabled"
 
 clear
-line "Tuned Node"
+line "TUNED NODE"
 display_and_run "oc project openshift-cluster-node-tuning-operator"
+line "Lets store node and pod name for future use:"
+display_and_run "node=\$(oc get nodes | grep -m 1 worker | cut -f 1 -d ' ') && echo \$node"
+display_and_run "pod=\$(oc get pods -n openshift-cluster-node-tuning-operator -o wide | grep \$node | cut -d ' ' -f 1) && echo \$pod"
 display_and_run "oc get pods -o wide"
+display_and_run "oc rsh \$pod tuned-adm active"
+display_and_run "oc rsh \$pod tuned-adm list"
+
+clear
+line "NODE TUNING OPERATOR"
+display_and_run "oc get tuned"
+display_and_run "oc edit tuned default"
+line "Lets get tuned CR files"
+display_and_run "wget https://raw.githubusercontent.com/skordas/demos/master/NTO/nf-conntrack-max.yaml https://raw.githubusercontent.com/skordas/demos/master/NTO/performance.yaml"
+display_and_run "vim nf-conntrack-max.yaml"
+display_and_run "oc create -f nf-conntrack-max.yaml"
+display_and_run "oc get tuned"
+display_and_run "oc edit tuned rendered"
+display_and_run "oc debug node/\$node -- chroot /host sysctl net.netfilter.nf_conntrack_max"
+display_and_run "oc label pod \$pod tuned.openshift.io/elasticsearch="
+display_and_run "oc debug node/\$node -- chroot /host sysctl net.netfilter.nf_conntrack_max"
+display_and_run "oc delete tuned nf-conntrack-max"
+display_and_run "oc debug node/\$node -- chroot /host sysctl net.netfilter.nf_conntrack_max"
+line "Matching not only using nodes or pods but also Machine Configuration"
+display_and_run "oc get machineconfig --show-labels"
+display_and_run "vim performance.yaml"
+
+clear
+line "Bonus: NTO as SLO"
+line "Node Tuning Operator as Second Line Operator"
+line "Operator can be in three states:"
+indent_line "- Managed"
+indent_line "- Unmanaged"
+indent_line "- Removed"
+display_and_run "oc get pods -n openshift-cluster-node-tuning-operator"
+display_and_run "oc patch tuned/default -p '{\"spec\":{\"managementState\": \"Removed\"}}' --type merge -n openshift-cluster-node-tuning-operator"
+display_and_run "oc get pods -n openshift-cluster-node-tuning-operator"
+display_and_run "oc patch tuned/default -p '{\"spec\":{\"managementState\": \"Managed\"}}' --type merge -n openshift-cluster-node-tuning-operator"
+display_and_run "oc get pods -n openshift-cluster-node-tuning-operator"
+
+clear
+line "Q&A"
+cleaning
---
+++ README.md
@@ -0,0 +1,11 @@
+# Demos
+
+## NTO
+Basic functionality of [Node Tuning Operator](https://github.com/openshift/cluster-node-tuning-operator) with some examples.
+
+To run demo without any problems you need to have [oc client](https://github.com/openshift/oc), [installed OCP](https://github.com/openshift/installer) and exported KUBECONFIG.
+To run demo just copy and paste into your terminal:
+
+```bash
+wget https://raw.githubusercontent.com/skordas/demos/master/NTO/nto_demo.sh && bash nto_demo.sh
+```
